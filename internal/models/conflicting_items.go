774002ddea37fa968692724bfc9de0bd7d96d92a
sjtw <simonjtweed@gmail.com>
2024-10-09 00:33:52 +0100

    conflicting item upsert

--- internal/models/conflicting_items.go
+++ internal/models/conflicting_items.go
@@ -1,5 +1,17 @@
 package models
 
-func UpsertConflictingItems(itemID string, conflictingItemID string) error {
+import "database/sql"
+
+type ConflictingItem struct {
+	ItemID            string `json:"item_id"`
+	ConflictingItemID string `json:"conflicting_item_id"`
+}
+
+func UpsertConflictingItems(tx *sql.Tx, itemID string, conflictingItemID string) error {
+	query := `INSERT INTO conflicting_items (item_id, conflicting_item_id) values ($1, $2);`
+	_, err := tx.Exec(query, itemID, conflictingItemID)
+	if err != nil {
+		return err
+	}
 	return nil
 }
